dcc418addd82823582d0581ad7f929bd3c334d46
PedroVidalDev <pedrohvidals@gmail.com>
2025-03-31 15:31:11 -0300

    chore(setup): create initial setup for styled component

--- App.tsx
+++ App.tsx
@@ -1,5 +1,44 @@
-import { Text } from 'react-native'
+/* eslint-disable camelcase */
+import theme from '@theme/index'
+import { Baloo2_700Bold } from '@expo-google-fonts/baloo-2'
+import styled, { css, ThemeProvider } from 'styled-components/native'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
 
 export default function App() {
-  return <Text> teste </Text>
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    Baloo2_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Title>Food Healthy....</Title>
+      </Container>
+    </ThemeProvider>
+  )
 }
+
+const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.COLORS.PURPLE};
+  align-items: center;
+  justify-content: center;
+`
+
+const Title = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.TITLE.LG}px;
+    font-family: ${theme.FONT_FAMILY.BALOO2.BALOO2_BOLD};
+    color: ${theme.COLORS.WHITE};
+  `}
+`
