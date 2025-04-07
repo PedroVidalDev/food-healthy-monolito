fecf6d6689a6b0b89dbdc72638fb903a075c492e
PedroVidalDev <pedrohvidals@gmail.com>
2025-04-06 22:27:37 -0300

    feat: add HomeHeader component to Home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,12 +5,15 @@ import { HomeFoodCategories } from './components/HomeFoodCategories'
 import { HomeFreeShippingCard } from './components/HomeFreeShippingCard'
 
 import { HomeContainer } from './styles'
+import { HomeHeader } from './components/HomeHeader'
 
 export const Home = () => {
   const insents = useSafeAreaInsets()
 
   return (
     <HomeContainer statusBarHeight={insents.top}>
+      <HomeHeader />
+
       <HomeFreeShippingCard />
 
       <HomeFoodCategories />
