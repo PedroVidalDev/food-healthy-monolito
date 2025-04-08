c5847c6c3109c838549f6c458e54292625cc8667
PedroVidalDev <pedrohvidals@gmail.com>
2025-04-08 10:37:56 -0300

    refactor: update Icon component for improved type safety and theme integration

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,11 +1,20 @@
 import * as PhosphorIcons from 'phosphor-react-native'
 
-import { IconProps } from './types'
+import theme from '@theme/index'
+import { IconComponentProps } from './types'
 
-export const Icon = (props: IconProps) => {
+export const Icon = (props: IconComponentProps) => {
   const { name = 'Cat', color, weight = 'regular', size = 24 } = props
 
-  const IconComponent = PhosphorIcons[name] as React.FC<IconProps>
+  const IconComponent = PhosphorIcons[name] as React.FC<PhosphorIcons.IconProps>
 
-  return <IconComponent color={color} weight={weight} size={size} />
+  const colorTheme = theme.COLORS[color]
+
+  return (
+    <IconComponent
+      color={colorTheme}
+      weight={weight as PhosphorIcons.IconWeight}
+      size={size}
+    />
+  )
 }
--- src/components/Icon/types.ts
+++ src/components/Icon/types.ts
@@ -2,7 +2,7 @@ import * as PhosphorIcons from 'phosphor-react-native'
 
 import theme from '@theme/index'
 
-export type IconProps = {
+export type IconComponentProps = {
   name?: keyof typeof PhosphorIcons
   color: keyof typeof theme.COLORS
   weight?: string
