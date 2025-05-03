4d1792983c1f1ce98650397301d4c28a539fef74
PedroVidalDev <pedrohvidals@gmail.com>
2025-05-02 22:08:04 -0300

    feat: create mocks for itens

---
+++ src/screens/Cart/components/ItemList/constants.ts
@@ -0,0 +1,22 @@
+import { ItemProps } from './components/Item/types'
+
+export const itemList: ItemProps[] = [
+  {
+    id: '1',
+    name: 'Café Expresso Tradicional',
+    price: 9.9,
+    quantity: 2,
+  },
+  {
+    id: '2',
+    name: 'Café Americano',
+    price: 8.5,
+    quantity: 1,
+  },
+  {
+    id: '3',
+    name: 'Café com Leite',
+    price: 10.0,
+    quantity: 3,
+  },
+]
