41b209c9b2ab118e736d9f43ce003dd0c1de074d
PedroVidalDev <pedrohvidals@gmail.com>
2025-05-11 12:46:15 -0300

    Initialize Spring projeto

---
+++ src/main/java/com/pedro/food_healthy/FoodHealthyApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.food_healthy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodHealthyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodHealthyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedro/food_healthy/FoodHealthyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.food_healthy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodHealthyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
