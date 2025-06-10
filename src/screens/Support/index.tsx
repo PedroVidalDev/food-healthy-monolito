a738cd414de91ed1e864b0ab5afbb66715cd0076
Natã <natamartins1@hotmail.com>
2025-06-09 21:29:18 -0300

    feat: creating support-screen and styles

---
+++ src/screens/Support/index.tsx
@@ -0,0 +1,45 @@
+import { Icon } from '@components/Icon'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import {
+  SupportContainer,
+  SupportDescription,
+  SupportDescriptionBanner,
+  SupportDescriptionTitle,
+  SupportHeader,
+  SupportOptions,
+  SupportOptionsText,
+  SupportOptionsTextContainer,
+  SupportOptionsTitle,
+  TextSupportDescription,
+  TextSupportHeader,
+} from './styles'
+
+export const Support = () => {
+  const insents = useSafeAreaInsets()
+  return (
+    <SupportContainer statusBarHeight={insents.top}>
+      <SupportHeader>
+        <Icon color="BLACK" name="ArrowArcLeft" size={16} />
+        <TextSupportHeader> Ajuda </TextSupportHeader>
+      </SupportHeader>
+
+      <SupportDescription>
+        <SupportDescriptionBanner>
+          <Icon name="PixLogo" color="GRAY_200" />
+          <SupportDescriptionTitle> Pagamentos </SupportDescriptionTitle>
+        </SupportDescriptionBanner>
+
+        <TextSupportDescription> Bla Bla Bla pagar </TextSupportDescription>
+      </SupportDescription>
+
+      <SupportOptions>
+        <Icon color="BLACK" name="ArrowArcRight" size={16} />
+
+        <SupportOptionsTextContainer>
+          <SupportOptionsTitle> Métodos aceitos </SupportOptionsTitle>
+          <SupportOptionsText> Lorem ipsum </SupportOptionsText>
+        </SupportOptionsTextContainer>
+      </SupportOptions>
+    </SupportContainer>
+  )
+}
---
+++ src/screens/Support/styles.ts
@@ -0,0 +1,71 @@
+import { ScrollView } from 'react-native'
+import styled, { css } from 'styled-components/native'
+
+export const SupportContainer = styled(ScrollView).attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingBottom: 148,
+    rowGap: 24,
+  },
+})<{ statusBarHeight: number }>`
+  ${({ theme, statusBarHeight }) => css`
+    flex: 1;
+    padding: ${statusBarHeight + 24}px 24px 0;
+    background-color: ${theme.COLORS.GRAY_900};
+  `}
+`
+export const SupportHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+`
+
+export const TextSupportHeader = styled.View`
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BALOO2.BALOO2_BOLD};
+  font-size: ${({ theme }) => theme.FONT_SIZE.LG}px;
+  color: ${({ theme }) => theme.COLORS.BLACK};
+`
+export const SupportDescription = styled.View`
+  width: 100%;
+  flex-direction: coluum;
+  align-items: center;
+`
+
+export const SupportDescriptionBanner = styled.View`
+  width: 100%
+  flex-direction: row;
+  color: ${({ theme }) => theme.COLORS.PRIMARY_DARK};
+`
+
+export const SupportDescriptionTitle = styled.View`
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BALOO2.BALOO2_SEMIBOLD};
+  font-size: ${({ theme }) => theme.FONT_SIZE.XG}px;
+`
+
+export const TextSupportDescription = styled.View`
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BALOO2.BALOO2_MEDIUM};
+  font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
+`
+
+export const SupportOptions = styled.View`
+  flex-direction = row;
+    color: ${({ theme }) => theme.COLORS.PRIMARY_DARK};
+  `
+
+export const SupportOptionsTextContainer = styled.View`
+  flex-direction = coluum;
+      color: ${({ theme }) => theme.COLORS.PRIMARY_LIGHT};
+
+  `
+export const SupportOptionsTitle = styled.View`
+  flex-direction: coluum:
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BALOO2.BALOO2_SEMIBOLD};
+  font-size: ${({ theme }) => theme.FONT_SIZE_MD}px;
+`
+
+export const SupportOptionsText = styled.View`
+  flex-direction: coluum:
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BALOO2.BALOO2_MEDIUM};
+  font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
+
+`
