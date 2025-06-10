0ded2de3b529149ea9e762357270d2f2f074ab65
PedroVidalDev <pedrohvidals@gmail.com>
2025-06-09 23:35:47 -0300

    feat: implement PaymentCardMethod component with input fields and update styles for better layout

--- src/screens/PaymentDetails/components/PaymentCardMethod/index.tsx
+++ src/screens/PaymentDetails/components/PaymentCardMethod/index.tsx
@@ -1,5 +1,15 @@
+import { Input } from '@components/Input'
 import { PaymentCardMethodContainer } from './styles'
+import { Button } from '@components/Button'
 
 export const PaymentCardMethod = () => {
-  return <PaymentCardMethodContainer></PaymentCardMethodContainer>
+  return (
+    <PaymentCardMethodContainer>
+      <Input label="Número do cartão" type="phone-pad" />
+      <Input label="Nome como impresso no cartão" type="phone-pad" />
+      <Input label="CVV" type="phone-pad" />
+      <Input label="Data de vencimento" type="phone-pad" />
+      <Button backgroundColor="PRIMARY" onPress={() => {}} text="Confirmar" />
+    </PaymentCardMethodContainer>
+  )
 }
--- src/screens/PaymentDetails/components/PaymentCardMethod/styles.ts
+++ src/screens/PaymentDetails/components/PaymentCardMethod/styles.ts
@@ -3,5 +3,5 @@ import styled from 'styled-components/native'
 export const PaymentCardMethodContainer = styled.View`
   width: 100%;
 
-  gap: 8px;
+  gap: 14px;
 `
